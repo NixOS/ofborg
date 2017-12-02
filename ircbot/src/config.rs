b557c480405e277c3bc79e1d5b5fb250bdecf808
Graham Christensen <graham@grahamc.com>
2017-12-01 22:51:01 -0500

    ok the bot does a thing now

---
+++ ircbot/src/config.rs
@@ -0,0 +1,86 @@
+use serde_json;
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+use irc::client::prelude::Config as IrcConfig;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    nickname: String,
+    alternate_nicknames: Vec<String>,
+    password: String,
+    channels: Vec<String>,
+    pub rabbitmq: RabbitMQConfig
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RabbitMQConfig {
+    pub ssl: bool,
+    pub host: String,
+    pub vhost: String,
+    pub username: String,
+    pub password: String,
+}
+
+
+impl RabbitMQConfig {
+    pub fn as_uri(&self) -> String{
+        return format!("{}://{}:{}@{}/{}",
+                       if self.ssl { "amqps" } else { "amqp" },
+                       self.username, self.password, self.host,
+                       self.vhost
+        );
+    }
+}
+
+impl Config {
+    pub fn irc_config(&self) -> IrcConfig {
+        IrcConfig {
+            nickname: Some(self.nickname.clone()),
+            nick_password: Some(self.password.clone()),
+            server: Some("chat.freenode.net".to_owned()),
+            port: Some(6697),
+            use_ssl: Some(true),
+            should_ghost: Some(true),
+
+            user_info: Some("a bot by Graham".to_owned()),
+            source: Some("https://github.com/grahamc/ofborg".to_owned()),
+            version: Some("lolidunno".to_owned()),
+
+            username: Some("graham".to_owned()),
+            realname: Some("Graham Christensen".to_owned()),
+            channels: Some(self.channels.clone()),
+
+            alt_nicks: Some(self.alternate_nicknames.clone()),
+            cert_path: None,
+            channel_keys: None,
+            encoding: None,
+            ghost_sequence: Some(vec!["RECOVER".to_owned(), "RELEASE".to_owned()]),
+
+            max_messages_in_burst: Some(5),
+            burst_window_length: Some(5),
+
+            ping_time: Some(180),
+            ping_timeout: Some(10),
+
+            use_mock_connection: Some(false),
+            mock_initial_value: None,
+
+            options: None,
+            owners: None,
+            password: Some("".to_owned()),
+            umodes: None,
+
+
+        }
+    }
+}
+
+pub fn load(src: &Path) -> Config {
+    let mut file = File::open(src).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    return serde_json::from_str(&contents).unwrap();
+}
--- ircbot/src/main.rs
+++ ircbot/src/main.rs
@@ -1,3 +1,114 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+extern crate irc;
+extern crate amqp;
+extern crate env_logger;
+
+#[macro_use]
+extern crate log;
+use irc::client::server::Server;
+use irc::client::prelude::Command;
+use irc::client::prelude::ServerExt;
+use irc::client::prelude::IrcServer;
+
+use amqp::protocol::basic::Deliver;
+use amqp::protocol::basic::BasicProperties;
+use amqp::Basic;
+use amqp::Channel;
+use amqp::Session;
+use amqp::Table;
+
+mod config;
+
+use std::thread;
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    target: String,
+    body: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(_) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", "info");
+        env_logger::init().unwrap();
+        info!("Defaulting RUST_LOG environment variable to info");
+    } else {
+        env_logger::init().unwrap();
+    }
+
+    let cfg = config::load(env::args().nth(1).unwrap().as_ref());
+
+    let mut session = Session::open_url(&cfg.rabbitmq.as_uri()).unwrap();
+    println!("Connected to rabbitmq");
+    println!("About to open channel #1");
+    let mut writechan = session.open_channel(1).unwrap();
+
+    let writeexch = writechan.exchange_declare(
+        "exchange-messages",  // exchange name
+        "fanout", //  exchange type
+        false, //
+        true, //
+        false, //
+        false,
+        false,
+        Table::new()
+    );
+
+
+    let mut readchan = session.open_channel(2).unwrap();
+    //queue: &str, passive: bool, durable: bool, exclusive: bool, auto_delete: bool, nowait: bool, arguments: Table
+    let readqueue = readchan.queue_declare("queue-publish", false, true, false, false, false, Table::new());
+
+    let server = IrcServer::from_config(cfg.irc_config()).unwrap();
+    server.identify().unwrap();
+    let reader = server.clone();
+
+    thread::spawn(move || {
+        let consumer_name = readchan.basic_consume(
+            move |_channel: &mut Channel, _deliver: Deliver, _headers: BasicProperties, body: Vec<u8>| {
+                let msg: Result<Message, serde_json::Error> = serde_json::from_slice(&body);
+                if let Ok(msg) = msg {
+                    server.send_privmsg(&msg.target, &msg.body).unwrap();
+                }
+            },
+            "queue-publish", "", false, true, false, false, Table::new());
+        println!("Starting consumer {:?}", consumer_name);
+        // server.stream().map(|m| print!("{}", m)).wait().count();
+
+        readchan.start_consuming();
+        readchan.close(200, "Bye").unwrap();
+        panic!("Lost the consumer!");
+    });
+
+
+    reader.for_each_incoming(|message| {
+        match message.command {
+            Command::PRIVMSG(ref target, ref msg) => {
+                let msg = serde_json::to_string(&Message{
+                    target: target.clone(),
+                    body: msg.clone(),
+                }).unwrap();
+
+                writechan.basic_publish(
+                    "exchange-messages".to_owned(),
+                    "".to_owned(),
+                    false,
+                    false,
+                    BasicProperties {
+                        content_type: Some("application/json".to_owned()),
+                        ..Default::default()
+                    },
+                    msg.into_bytes()
+                ).expect("Failed to publish message");
+            }
+            _ => {
+                print!("{:?}\n", message.command);
+            },
+        }
+    }).unwrap();
 }
