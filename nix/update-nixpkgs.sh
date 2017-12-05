fd08bce899df11181d2d174cbccdb5f71bbc37f9
Graham Christensen <graham@grahamc.com>
2017-12-04 20:55:25 -0500

    Update nixpkgs

--- nix/update-nixpkgs.sh
+++ nix/update-nixpkgs.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nix-prefetch-git
 
 nix-prefetch-git https://github.com/nixos/nixpkgs-channels.git \
                  --rev refs/heads/nixpkgs-unstable > ./nix/nixpkgs.json
