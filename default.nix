8c9e63d12c5ea4c7afa0bd757bf1b24e246e0d8e
Graham Christensen <graham@grahamc.com>
2017-12-04 21:30:15 -0500

    Move all the PHP stuff to a subdir

--- default.nix
+++ default.nix
@@ -12,4 +12,24 @@ let
 in {
   ofborg.rs = stripDeps (pkgs.callPackage ./nix/ofborg-carnix.nix {}).ofborg_0_1_0;
   ircbot = stripDeps (pkgs.callPackage ./nix/ircbot-carnix.nix {}).ircbot_0_1_0;
+
+  # /nix/store/a4xfsgh5bwh5w4m9y1j40ry4dq892gl4-ofborg/
+  ofborg.php = pkgs.runCommand
+    "ofborg"
+    {
+      src = builtins.filterSource
+        (path: type: !(
+             (type == "symlink" && baseNameOf path == "result")
+          || (type == "directory" && baseNameOf path == ".git")
+        ))
+        ./php;
+    }
+    ''
+      cp -r $src ./ofborg
+      chmod -R u+w ./ofborg
+      cd ofborg
+      ls -la
+      cd ..
+      mv ofborg $out
+    '';
 }
