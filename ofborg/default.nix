a5a9618c31c52948447972ebd0ff15cc88d40147
Graham Christensen <graham@grahamc.com>
2018-01-28 18:06:58 -0500

    Delete crates.nix / default.nix to work

--- ofborg/default.nix
+++ ofborg/default.nix
@@ -1,3 +1,2 @@
-## To update dependencies use:
-# $ carnix -o crates.nix --src ./. Cargo.lock --standalone
-(import ./crates.nix).ofborg_0_1_0
+## To update dependencies, use ../nix/update-carnix.sh
+(import ../default.nix {}).ofborg.rs
