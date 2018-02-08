d466b9d3b75a42cfedd3346ea3e21d79e5fc04c6
Graham Christensen <graham@grahamc.com>
2018-02-08 15:17:43 -0500

    Use a patched carnix emission with a smarter include

--- nix/update-carnix.sh
+++ nix/update-carnix.sh
@@ -2,5 +2,13 @@
 
 cd nix
 
-carnix ./../ofborg/Cargo.lock  --output ./ofborg-carnix.nix
-carnix ./../ircbot/Cargo.lock  --output ./ircbot-carnix.nix
+patched_carnix() {
+    src=$1
+    result=$2
+
+    carnix "$src"  --output "$result"
+    patch -p1 "$result" ./carnix.patch
+}
+
+patched_carnix ./../ofborg/Cargo.lock ./ofborg-carnix.nix
+patched_carnix ./../ircbot/Cargo.lock ./ircbot-carnix.nix
