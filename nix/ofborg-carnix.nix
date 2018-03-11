b596e4a96a64cc4c3f8a02a514b027a7f4210fad
Graham Christensen <graham@grahamc.com>
2018-03-10 19:09:05 -0500

    d'oh

--- nix/ofborg-carnix.nix
+++ nix/ofborg-carnix.nix
@@ -413,9 +413,9 @@ rec {
     sha256 = "1y6qnd9r8ga6y8mvlabdrr73nc8cshjjlzbvnanzyj9b8zzkfwk2";
     inherit dependencies buildDependencies features;
   };
-  ofborg_0_1_2_ = { dependencies?[], buildDependencies?[], features?[] }: buildRustCrate {
+  ofborg_0_1_3_ = { dependencies?[], buildDependencies?[], features?[] }: buildRustCrate {
     crateName = "ofborg";
-    version = "0.1.2";
+    version = "0.1.3";
     authors = [ "Graham Christensen <graham@grahamc.com>" ];
     src = include [ "Cargo.toml" "Cargo.lock" "src" "test-srcs" "build.rs" ] ./../ofborg;
     build = "build.rs";
@@ -1342,10 +1342,10 @@ rec {
     libc_0_2_36.default = true;
     num_cpus_1_8_0.default = (f.num_cpus_1_8_0.default or true);
   }) [ libc_0_2_36_features ];
-  ofborg_0_1_2 = { features?(ofborg_0_1_2_features {}) }: ofborg_0_1_2_ {
+  ofborg_0_1_3 = { features?(ofborg_0_1_3_features {}) }: ofborg_0_1_3_ {
     dependencies = mapFeatures features ([ amqp_0_1_0 either_1_4_0 env_logger_0_4_3 fs2_0_4_3 hubcaps_0_3_16 hyper_0_10_13 hyper_native_tls_0_2_4 log_0_3_8 lru_cache_0_1_1 md5_0_3_6 serde_1_0_27 serde_derive_1_0_27 serde_json_1_0_9 tempfile_2_2_0 uuid_0_4_0 ]);
   };
-  ofborg_0_1_2_features = f: updateFeatures f (rec {
+  ofborg_0_1_3_features = f: updateFeatures f (rec {
     amqp_0_1_0.default = true;
     either_1_4_0.default = true;
     env_logger_0_4_3.default = true;
@@ -1356,7 +1356,7 @@ rec {
     log_0_3_8.default = true;
     lru_cache_0_1_1.default = true;
     md5_0_3_6.default = true;
-    ofborg_0_1_2.default = (f.ofborg_0_1_2.default or true);
+    ofborg_0_1_3.default = (f.ofborg_0_1_3.default or true);
     serde_1_0_27.default = true;
     serde_derive_1_0_27.default = true;
     serde_json_1_0_9.default = true;
