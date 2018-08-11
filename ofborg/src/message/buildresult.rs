5797b28e04692ae02716375945e5d544cb780198
Daiderd Jordan <daiderd@gmail.com>
2018-08-11 16:43:21 +0200

    buildresult: add v1 format
    This drops the old build success format and makes the request_id
    mandatory (was optional for compatibility).

--- ofborg/src/message/buildresult.rs
+++ ofborg/src/message/buildresult.rs
@@ -24,16 +24,33 @@ pub struct LegacyBuildResult {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub enum V1Tag {
+    V1
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
 pub enum BuildResult {
-    // TODO: introduce V1, V2, etc. and enable internal tagging.
+    V1 {
+        tag: V1Tag,  // use serde once all enum variants have a tag
+        repo: Repo,
+        pr: Pr,
+        system: String,
+        output: Vec<String>,
+        attempt_id: String,
+        request_id: String,
+        // removed success
+        status: BuildStatus,
+        skipped_attrs: Option<Vec<String>>,
+        attempted_attrs: Option<Vec<String>>,
+    },
     Legacy {
         repo: Repo,
         pr: Pr,
         system: String,
         output: Vec<String>,
         attempt_id: String,
-        request_id: Option<String>,
+        request_id: String,
         success: Option<bool>, // replaced by status
         status: Option<BuildStatus>,
         skipped_attrs: Option<Vec<String>>,
@@ -58,7 +75,19 @@ impl BuildResult {
                     status: self.status(),
                     attempted_attrs: attempted_attrs.to_owned(),
                     skipped_attrs: skipped_attrs.to_owned(),
-                }
+                },
+            &BuildResult::V1 { ref repo, ref pr, ref system, ref output, ref attempt_id, ref request_id, ref attempted_attrs, ref skipped_attrs, .. } =>
+                LegacyBuildResult {
+                    repo: repo.to_owned(),
+                    pr: pr.to_owned(),
+                    system: system.to_owned(),
+                    output: output.to_owned(),
+                    attempt_id: attempt_id.to_owned(),
+                    request_id: request_id.to_owned(),
+                    status: self.status(),
+                    attempted_attrs: attempted_attrs.to_owned(),
+                    skipped_attrs: skipped_attrs.to_owned(),
+                },
         }
     }
 
@@ -72,7 +101,9 @@ impl BuildResult {
                         &Some(true) => BuildStatus::Success,
                         &Some(false) => BuildStatus::Failure,
                     }
-                })
+                }),
+            &BuildResult::V1 { ref status, .. } =>
+                status.to_owned(),
         }
     }
 }
@@ -82,6 +113,15 @@ impl BuildResult {
 mod tests {
     use super::*;
     use serde_json;
+
+    #[test]
+    fn v1_serialization() {
+        let input = r#"{"tag":"V1","repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":["unpacking sources"],"attempt_id":"attempt-id-foo","request_id":"bogus-request-id","status":"Success","skipped_attrs":["AAAAAASomeThingsFailToEvaluate"],"attempted_attrs":["hello"]}"#;
+        let result: BuildResult = serde_json::from_str(input).expect("result required");
+        assert_eq!(result.status(), BuildStatus::Success);
+        let output = serde_json::to_string(&result).expect("json required");
+        assert_eq!(output, r#"{"tag":"V1","repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":["unpacking sources"],"attempt_id":"attempt-id-foo","request_id":"bogus-request-id","status":"Success","skipped_attrs":["AAAAAASomeThingsFailToEvaluate"],"attempted_attrs":["hello"]}"#, "json of: {:?}", result);
+    }
 
     #[test]
     fn legacy_serialization() {
@@ -94,11 +134,11 @@ mod tests {
 
     #[test]
     fn legacy_none_serialization() {
-        let input = r#"{"repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":[],"attempt_id":"attempt-id-foo"}"#;
+        let input = r#"{"repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":[],"attempt_id":"attempt-id-foo","request_id":"bogus-request-id"}"#;
         let result: BuildResult = serde_json::from_str(input).expect("result required");
         assert_eq!(result.status(), BuildStatus::Skipped);
         let output = serde_json::to_string(&result).expect("json required");
-        assert_eq!(output, r#"{"repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":[],"attempt_id":"attempt-id-foo","request_id":null,"success":null,"status":null,"skipped_attrs":null,"attempted_attrs":null}"#, "json of: {:?}", result);
+        assert_eq!(output, r#"{"repo":{"owner":"NixOS","name":"nixpkgs","full_name":"NixOS/nixpkgs","clone_url":"https://github.com/nixos/nixpkgs.git"},"pr":{"target_branch":"master","number":42,"head_sha":"0000000000000000000000000000000000000000"},"system":"x86_64-linux","output":[],"attempt_id":"attempt-id-foo","request_id":"bogus-request-id","success":null,"status":null,"skipped_attrs":null,"attempted_attrs":null}"#, "json of: {:?}", result);
     }
 
     #[test]
