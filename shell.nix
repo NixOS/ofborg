87974028d1692710244f866a35db8d628aad66c9
Graham Christensen <graham@grahamc.com>
2019-01-02 20:06:50 -0500

    build phase for rust pedantry

--- shell.nix
+++ shell.nix
@@ -35,6 +35,40 @@ let
     # HISTFILE = "${src}/.bash_hist";
   };
 
+  mozilla-rust-overlay = stdenv.mkDerivation (rec {
+    name = "gh-event-forwarder";
+    buildInputs = with pkgs; [
+      bash
+      nix-prefetch-git
+      rust.rustc
+      rust.cargo
+      #rustfmt
+      #carnix
+      openssl.dev
+      pkgconfig
+      git
+    ]
+      ++ stdenv.lib.optional useNix1 oldpkgs.nix1
+      ++ stdenv.lib.optional stdenv.isDarwin pkgs.darwin.Security;
+
+    postHook = ''
+      checkPhase() {
+          ( cd "${builtins.toString ./.}/ofborg" && cargo build && cargo test)
+      }
+    '';
+
+    HISTFILE = "${toString ./.}/.bash_hist";
+    RUSTFLAGS = "-D warnings";
+    RUST_BACKTRACE = "1";
+    RUST_LOG = "ofborg=debug";
+    NIX_PATH = "nixpkgs=${pkgs.path}";
+    passthru.phpEnv = phpEnv;
+  }
+  // stdenv.lib.optionalAttrs stdenv.isLinux {
+    LOCALE_ARCHIVE_2_21 = "${oldpkgs.glibcLocales}/lib/locale/locale-archive";
+    LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  });
+
   rustEnv = stdenv.mkDerivation (rec {
     name = "gh-event-forwarder";
     buildInputs = with pkgs; [
