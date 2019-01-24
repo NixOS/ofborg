6ddf6193343e01d6426fbe2338651c97757f5da7
Graham Christensen <graham@grahamc.com>
2019-01-24 16:50:03 -0500

    Create a type for queue job system types, and express build jobs destinations in that form

---
+++ ofborg/src/systems.rs
@@ -0,0 +1,19 @@
+pub enum System {
+    X8664Linux,
+    Aarch64Linux,
+    X8664Darwin,
+}
+
+impl System {
+    pub fn to_string(&self) -> String {
+        match self {
+            System::X8664Linux => String::from("x86_64-linux"),
+            System::Aarch64Linux => String::from("aarch64-linux"),
+            System::X8664Darwin => String::from("x86_64-darwin"),
+        }
+    }
+
+    pub fn as_build_destination(&self) -> (Option<String>, Option<String>) {
+        (None, Some(format!("build-inputs-{}", self.to_string())))
+    }
+}
