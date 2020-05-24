bf41d6c0350d877576c00262cafd381d7f70b5dd
Daiderd Jordan <daiderd@gmail.com>
2020-05-24 16:42:15 +0200

    enforce Cargo.lock is up to date instead of building

--- nix/update-crates.sh
+++ nix/update-crates.sh
@@ -2,5 +2,5 @@
 #!nix-shell -i bash -E "with import <nixpkgs> {}; mkShell { nativeBuildInputs = [ (import (builtins.fetchTarball https://github.com/kolloch/crate2nix/archive/0.8.0.tar.gz) {}) ]; }"
 set -e
 
-cargo build
+cargo fetch --locked
 crate2nix generate
