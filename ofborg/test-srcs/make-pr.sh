f7bc596e31e1e4e529e17c91855522347498919d
Alyssa Ross <hi@alyssa.is>
2021-08-02 09:48:29 +0000

    Don't use global git config in tests
    The tests were failing for me because I have git configured to use
    "main" instead of "master" as the default initial branch name for new
    repositories.  Having the tests use global git configuration from
    developer systems is just asking for trouble with non-reproducibility,
    but fortunately git gives us a mechanism to disable it.
    
    This also means we don't need to worry about GPG any more.

--- ofborg/test-srcs/make-pr.sh
+++ ofborg/test-srcs/make-pr.sh
@@ -4,19 +4,22 @@ set -eu
 bare=$1
 co=$2
 
+export GIT_CONFIG_GLOBAL=/dev/null
+export GIT_CONFIG_NOSYSTEM=1
+
 makepr() {
     git init --bare "$bare"
     git clone "$bare" "$co"
 
     cp build/* "$co/"
     git -C "$co" add .
-    git -C "$co" commit --no-gpg-sign --author "GrahamCOfBorg <graham+cofborg@example.com>" -m "initial repo commit"
+    git -C "$co" commit --author "GrahamCOfBorg <graham+cofborg@example.com>" -m "initial repo commit"
     git -C "$co" push origin master
 
     cp build-pr/*  "$co/"
     git -C "$co" checkout -b my-cool-pr
     git -C "$co" add .
-    git -C "$co" commit --no-gpg-sign --author "GrahamCOfBorg <graham+cofborg@example.com>" -m "check out this cool PR"
+    git -C "$co" commit --author "GrahamCOfBorg <graham+cofborg@example.com>" -m "check out this cool PR"
     git -C "$co" push origin my-cool-pr:refs/pull/1/head
 }
 
