db5293a4fb219d69f5734d87af573e1940396587
Jörg Thalheim <joerg@thalheim.io>
2022-01-02 18:21:33 +0100

    eval aarch64-darwin rather than i686-linux
    I don't think anyone still runs nixpkgs-review on 32-bit x86.

--- ofborg/src/outpaths.nix
+++ ofborg/src/outpaths.nix
@@ -11,8 +11,8 @@ let
     {
       supportedSystems = [
         "aarch64-linux"
-        # "aarch64-darwin" # !!!
-        "i686-linux"
+        "aarch64-darwin"
+        #"i686-linux"  # !!!
         "x86_64-linux"
         "x86_64-darwin"
       ];
