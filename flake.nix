d531c5e50829bbd02f06f11f81094e383ab14e6f
Artturin <Artturin@artturin.com>
2023-08-14 19:22:21 +0300

    bump nixpkgs to 23.05
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/7a6a010c3a1d00f8470a5ca888f2f927f1860a19' (2022-12-08)
      → 'github:nixos/nixpkgs/720e61ed8de116eec48d6baea1d54469b536b985' (2023-08-13)
    • Updated input 'nixpkgs-for-php':
        'github:nixos/nixpkgs/99ec06122f481588abafd91f2710d80a5320efe6' (2022-12-08)
      → 'github:nixos/nixpkgs/380be19fbd2d9079f677978361792cb25e8a3635' (2023-05-31)
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/720e61ed8de116eec48d6baea1d54469b536b985' (2023-08-13)
      → 'github:nixos/nixpkgs/32dcb45f66c0487e92db8303a798ebc548cadedc' (2023-09-30)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-for-php.url = "github:nixos/nixpkgs/nixos-22.05";
   };
 
