c21c673e0d0c4ffb9aa6fc353e79063447fc7f39
Cole Helbling <cole.e.helbling@outlook.com>
2024-05-19 13:11:25 -0700

    fixup: flake-compat

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 (import
   (fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
-    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    url = "https://github.com/edolstra/flake-compat/archive/0f9255e01c2351cc7d116c072cb317785dd33b33.tar.gz";
+    sha256 = "0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
   })
   { src = ./.; }).defaultNix.packages.${builtins.currentSystem}
